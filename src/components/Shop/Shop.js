fc0d335a59e4c1cd9512cf551e229433ed5cb731
sharif764 <ss1884088@gmail.com>
2021-09-23 01:18:03 +0600

    JJ

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,17 +8,21 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
+    const handleAddToCart = (product) => {
+        console.log(product.name);
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
-                <h3>Products : {products.length}</h3>
                 {
-                    products.map(product => <Product product={product}></Product>)
+                    products.map(product => <Product key={product.key}
+                        product={product}
+                        handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h3>Order Summary</h3>
-                <h5>Items Order</h5>
+                <h5>Items Order :</h5>
             </div>
         </div>
     );
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Product.css';
 const Product = (props) => {
-    console.log(props.product);
+
     const { img, name, key, price, priceFraction, seller, shipping, start, stock, url } = props.product;
     return (
         <div className="product">
@@ -11,6 +11,7 @@ const Product = (props) => {
                 <p><small>by :{seller}</small></p>
                 <p>Price : {price}</p>
                 <p>only {stock} left in stock - order soon</p>
+                <button onClick={() => props.handleAddToCart(props.product)} className="btn-regular">add to cart</button>
             </div>
         </div>
     );
